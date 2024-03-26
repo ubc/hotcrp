ca7f70b05b5ecae791cf7a8815d0e50c2068b880
ubc-tuehoang <tuehoang@yahoo.com>
2024-03-26 15:25:10 -0700

    removed all tests to reduce noise and check each one

--- test/check.sh
+++ test/check.sh
@@ -13,12 +13,12 @@ runcheck () {
 }
 
 #runcheck test/test01.php "$@"
-runcheck test/test02.php "$@"
-runcheck test/test03.php "$@"
-runcheck test/test04.php "$@"
+#runcheck test/test02.php "$@"
+#runcheck test/test03.php "$@"
+#runcheck test/test04.php "$@"
 #runcheck test/test05.php "$@"
-runcheck test/test06.php "$@"
-runcheck test/test07.php "$@"
+#runcheck test/test06.php "$@"
+#runcheck test/test07.php "$@"
 #if $all; then
 #    runcheck test/test08.php "$@"
 #fi
