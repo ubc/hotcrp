37c164597a5f701f1e407d16916936ff98e580ed
ubc-tuehoang <tuehoang@yahoo.com>
2024-03-26 15:19:56 -0700

    test removal of test01,05,08

--- test/check.sh
+++ test/check.sh
@@ -12,15 +12,15 @@ runcheck () {
     if [ "$a" = 0 ]; then a=$z; fi
 }
 
-runcheck test/test01.php "$@"
+#runcheck test/test01.php "$@"
 runcheck test/test02.php "$@"
 runcheck test/test03.php "$@"
 runcheck test/test04.php "$@"
-runcheck test/test05.php "$@"
+#runcheck test/test05.php "$@"
 runcheck test/test06.php "$@"
 runcheck test/test07.php "$@"
-if $all; then
-    runcheck test/test08.php "$@"
-fi
+#if $all; then
+#    runcheck test/test08.php "$@"
+#fi
 
 exit $a
