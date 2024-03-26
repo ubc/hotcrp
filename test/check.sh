9225167cba1dee1cf8ed291b4d828707a5acafdb
ubc-tuehoang <tuehoang@yahoo.com>
2024-03-26 16:05:51 -0700

    added test04 back to check.sh

--- test/check.sh
+++ test/check.sh
@@ -15,7 +15,7 @@ runcheck () {
 runcheck test/test01.php "$@"
 runcheck test/test02.php "$@"
 runcheck test/test03.php "$@"
-#runcheck test/test04.php "$@"
+runcheck test/test04.php "$@"
 #runcheck test/test05.php "$@"
 #runcheck test/test06.php "$@"
 #runcheck test/test07.php "$@"
