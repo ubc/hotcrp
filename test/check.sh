3dd8b902e727e5d00715d2e60006795e4c83475d
ubc-tuehoang <tuehoang@yahoo.com>
2024-03-26 15:54:23 -0700

    added test02 back to the check.sh list

--- test/check.sh
+++ test/check.sh
@@ -13,7 +13,7 @@ runcheck () {
 }
 
 runcheck test/test01.php "$@"
-#runcheck test/test02.php "$@"
+runcheck test/test02.php "$@"
 #runcheck test/test03.php "$@"
 #runcheck test/test04.php "$@"
 #runcheck test/test05.php "$@"
